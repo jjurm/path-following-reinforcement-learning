904474f0183a5c83685076c193addb9ffa26972c
jjurm <micko.jura64@gmail.com>
2020-03-03 00:23:11 +0000

    initialise small nn weigths

--- models/actor.py
+++ models/actor.py
@@ -1,15 +1,18 @@
-from keras.models import Sequential
+from keras.initializers import RandomNormal
 from keras.layers import Dense, Flatten
+from keras.models import Sequential
 
 
 def build_actor(batch_size, nb_actions, env):
     """
     Docs
     """
+    init = RandomNormal(mean=0.0, stddev=0.1, seed=None)
     actor = Sequential([
         Flatten(input_shape=(batch_size,) + env.observation_space.shape),
-        Dense(8, activation='relu'),
-        Dense(nb_actions, activation='linear'),
+        Dense(8, activation='relu', kernel_initializer=init),
+        Dense(8, activation='relu', kernel_initializer=init),
+        Dense(nb_actions, activation='tanh', kernel_initializer=init),
     ])
     print(actor.summary())
 
--- models/critic.py
+++ models/critic.py
@@ -1,16 +1,19 @@
+from keras.initializers import RandomNormal
+from keras.layers import Dense, Flatten, Concatenate
 from keras.models import Model
-from keras.layers import Dense, Flatten, Input, Concatenate
+
 
 def build_critic(action_input, observation_input):
     """
     Docs
     """
+    init = RandomNormal(mean=0.0, stddev=0.1, seed=None)
     flattened_observation = Flatten()(observation_input)
     x = Concatenate()([action_input, flattened_observation])
-    x = Dense(32, activation='relu')(x)
-    x = Dense(1, activation='linear')(x)
+    x = Dense(8, activation='relu', kernel_initializer=init)(x)
+    x = Dense(8, activation='relu', kernel_initializer=init)(x)
+    x = Dense(1, activation='linear', kernel_initializer=init)(x)
     critic = Model(inputs=[action_input, observation_input], outputs=x)
-
     print(critic.summary())
 
     return critic
