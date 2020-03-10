40c797afd8d7812dbef20c45a77af4d9f89c9540
kwotsin <leeks_50996@hotmail.com>
2020-03-10 16:19:50 +0000

    keras

--- main.py
+++ main.py
@@ -28,9 +28,12 @@ nb_actions = env.action_space.shape[0]
 #   Parameters
 ################
 # Training parameters
-batch_size = 8
-lr = 1e-3
-max_episode_steps = 200
+batch_size = 64
+lr = 1e-4
+max_episode_steps = 100
+limit = 100000
+nb_steps = 10000
+
 # Agent parameters
 num_steps_warmup_critic = 100
 num_steps_warmup_actor = 100
@@ -41,22 +44,30 @@ gamma = 0.99
 ###############
 action_input = Input(shape=(nb_actions,), name='action_input')
 observation_input = Input(shape=(batch_size,) + env.observation_space.shape, name='observation_input')
+num_feat = 32
+
+# print(env.observation_space.shape)
+# print(nb_actions)
+# assert False
 
 actor = actor.build_actor(
     batch_size=batch_size,
     nb_actions=nb_actions,
-    env=env)
+    env=env,
+    num_feat=num_feat)
 
 critic = critic.build_critic(
     action_input=action_input,
-    observation_input=observation_input)
+    observation_input=observation_input,
+    num_feat=num_feat)
 
 # Optimizer
-opt = Adam(lr=lr, clipnorm=1.)
+opt = Adam(lr=lr, clipnorm=1.0)
+# opt = Adam(lr=lr, clipnorm=0.01)
 
 # Build and compile agent
 memory = SequentialMemory(
-    limit=100000,
+    limit=limit,
     window_length=batch_size)
 
 random_process = OrnsteinUhlenbeckProcess(
@@ -79,19 +90,18 @@ agent = DDPGAgent(
 
 agent.compile(opt, metrics=['mae'])
 
-#%%
-
 history = agent.fit(
     env,
-    nb_steps=2000,
+    nb_steps=nb_steps,
     visualize=True,
     verbose=0,
     nb_max_episode_steps=max_episode_steps)
 
-# # After training is done, we save the final weights.
-# agent.save_weights('ddpg_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+print(history)
 
-#%%
+# After training is done, we save the final weights.
+agent.save_weights('ddpg_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+
 
 # Finally, evaluate our algorithm for 5 episodes.
 agent.test(
---
+++ spinning.py
@@ -0,0 +1,55 @@
+# from spinup.utils.run_utils import ExperimentGrid
+# from spinup import ppo_pytorch
+# import torch
+
+# if __name__ == '__main__':
+#     import argparse
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument('--cpu', type=int, default=4)
+#     parser.add_argument('--num_runs', type=int, default=3)
+#     args = parser.parse_args()
+
+#     eg = ExperimentGrid(name='ppo-pyt-bench')
+#     eg.add('env_name', 'CartPole-v0', '', True)
+#     eg.add('seed', [10*i for i in range(args.num_runs)])
+#     eg.add('epochs', 10)
+#     eg.add('steps_per_epoch', 4000)
+#     eg.add('ac_kwargs:hidden_sizes', [(32,), (64,64)], 'hid')
+#     eg.add('ac_kwargs:activation', [torch.nn.Tanh, torch.nn.ReLU], '')
+#     eg.run(ppo_pytorch, num_cpu=args.cpu)
+
+
+#========
+
+from spinup.utils.run_utils import ExperimentGrid
+from spinup import ppo_pytorch, ddpg_pytorch, sac_pytorch
+import torch
+import gym
+
+if __name__ == '__main__':
+    ############
+    #   Setup
+    ############
+    ENV_NAME = 'robot_env:robot-env-v0'
+    env = gym.make(ENV_NAME)
+    feat = 32
+
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--cpu', type=int, default=4)
+    parser.add_argument('--num_runs', type=int, default=3)
+    args = parser.parse_args()
+
+    eg = ExperimentGrid(name='ppo-pyt-bench')
+    eg.add('env_name', ENV_NAME, '', True)
+    eg.add('seed', [10*i for i in range(args.num_runs)])
+    eg.add('epochs', 20)
+    eg.add('steps_per_epoch', 4000)
+    # eg.add('ac_kwargs:hidden_sizes', [(feat,), (feat*2,feat*2)], 'hid')
+    # eg.add('ac_kwargs:activation', [torch.nn.Tanh, torch.nn.ReLU], '')
+    eg.add('ac_kwargs:hidden_sizes', [(feat*2,feat*2)], 'hid')
+    eg.add('ac_kwargs:activation', [torch.nn.ReLU], '')
+
+    # eg.run(ppo_pytorch, num_cpu=args.cpu)
+    eg.run(ddpg_pytorch, num_cpu=args.cpu)
